18dd3653a60d43ab60f3cceda7ff9d835824dd28
Mike Hsieh <mythsieh@gmail.com>
2015-07-28 18:47:38 -0700

    Passing tests - excluding objects & nested arrays NO RECURSION

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 
+/*  TEST CASES (NON RECURSIVE passed)
+9,
+null,
+true,
+false,
+"Hello world",
+[],
+[8],
+["hi"],
+[8, "hi"],
+[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]
+*/
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof target === "string") {
+    return "\"" + target + "\"";
+  }
+  if (Array.isArray(target)) {
+    var result = '[';
+      for (var i = 0; i < target.length; i++) {
+        if (typeof target[i] === "string") {
+          result += "\"" + target[i] + "\"" ;
+        } else {
+          result += target[i];
+        }
+        if (i < target.length - 1) {
+          result += ',';
+        }
+      }
+      return result += ']';
+  }
+  return "" + target;
 };
