4389c28eb73ef233f0c380c2d1e8e8c514fa7b0c
Mike Hsieh <mythsieh@gmail.com>
2015-07-28 20:11:36 -0700

    StringifyJSON.js -- added case for array, recursive

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,12 +3,20 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-    //Base cases
-    if (typeof target === "string") {
-        return "\"" + target + "\"";
-    } else if (target === null || typeof target === "number" || typeof target === "boolean") {
-        return "" + target;
-    } else if (target === undefined) {
-        return undefined;
-    }
+  if (typeof target === "string") {
+      return "\"" + target + "\"";
+  } else if (target === null || typeof target === "number" || typeof          target === "boolean") {
+      return "" + target;
+  } else if (target === undefined) {
+      return undefined;
+  } else if (Array.isArray(target)) {
+      var result = '[';
+      for(var i = 0; i < target.length; i++) {
+          result += rStringTransform(target[i]);
+          if (i < target.length - 1) {
+              result += ',';
+          }
+      }
+      return result += ']';
+  }
 };
