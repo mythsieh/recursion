69f506f73940ebdf813495afe6d48726777ef1bd
Mike Hsieh <mythsieh@gmail.com>
2015-07-28 19:21:45 -0700

    StringifyJSON - Recursion - Base cases defined - string, null, number, boolean added, undefined

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,37 +2,13 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
-/*  TEST CASES (NON RECURSIVE passed)
-9,
-null,
-true,
-false,
-"Hello world",
-[],
-[8],
-["hi"],
-[8, "hi"],
-[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]
-*/
-
 var stringifyJSON = function(obj) {
-  if (typeof target === "string") {
-    return "\"" + target + "\"";
-  }
-  if (Array.isArray(target)) {
-    var result = '[';
-      for (var i = 0; i < target.length; i++) {
-        if (typeof target[i] === "string") {
-          result += "\"" + target[i] + "\"" ;
-        } else {
-          result += target[i];
-        }
-        if (i < target.length - 1) {
-          result += ',';
-        }
-      }
-      return result += ']';
-  }
-  return "" + target;
+    //Base cases
+    if (typeof target === "string") {
+        return "\"" + target + "\"";
+    } else if (target === null || typeof target === "number" || typeof target === "boolean") {
+        return "" + target;
+    } else if (target === undefined) {
+        return undefined;
+    }
 };
