860152511277a11e8dc1abd53a32b0e0b13b08bf
Mike Hsieh <mythsieh@gmail.com>
2015-07-30 12:51:13 -0700

    getElementsByClassName.js - updated easy case example to pass test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,11 @@
 // If life was easy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
+//   var arr = document.getElementsByClassName(className);
+//   return Array.prototype.slice.apply(arr);
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  return document.getElementsByClassName(className);
 };
